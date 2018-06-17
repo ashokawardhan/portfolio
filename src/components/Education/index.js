617d0f921ee6efafe554066c62acb575e5121ee3
Ashoka Chandra Wardhan <awardhan@expedia.com>
2018-06-17 13:54:09 +0530

    Create education timeline

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
-import Paper from '@material-ui/core/Paper';
 import SectionContainer from 'src/components/commons/SectionContainer';
-
-const StyledPaper = styled(Paper)`
-    margin-top: 20px;
-    margin-bottom: 20px;
-    margin-left: 10px;
-    margin-right: 10px;
-    padding: 10px 10px 40px 10px;
-    background-color: #FFF;
-`;
-
-const PaperContainer = (props) => (
-    <StyledPaper
-        elevation={20}
-    >
-        {props.children}
-    </StyledPaper>
-);
+import TimelineContent from 'src/components/Education/timeline-content';
+import './timeline.css';
 
 const Project = () => (
     <SectionContainer id="education" title="Education">
-        <PaperContainer>
-            <div />
-        </PaperContainer>
+        <TimelineContent />
     </SectionContainer>
 );
 
